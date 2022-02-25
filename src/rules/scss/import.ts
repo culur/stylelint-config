608ad2f227821f5ce866155bedcbbfcff755fc81
phamhongphuc <phamhongphuc.info@gmail.com>
2022-02-25 17:13:58 +0700

    feat(scss): remove duplicated rules `import`
    https://github.com/stylelint-scss/stylelint-config-recommended-scss/blob/master/index.js

--- src/rules/scss/import.ts
+++
@@ -1,7 +0,0 @@
-import type { Config } from 'stylelint';
-
-export const scssImport: Config['rules'] = {
-    'scss/at-import-partial-extension': 'never',
-    'scss/at-import-no-partial-leading-underscore': true,
-    'scss/partial-no-import': true,
-};
--- src/scss.ts
+++ src/scss.ts
@@ -1,6 +1,5 @@
 import type { Config } from 'stylelint';
 import { atRuleNoUnknown } from './rules/at-rule-no-unknown';
-import { scssImport } from './rules/scss/import';
 
 const config: Config = {
     extends: ['./css'],
@@ -8,7 +7,6 @@ const config: Config = {
     rules: {
         'at-rule-no-unknown': null,
         'scss/at-rule-no-unknown': atRuleNoUnknown,
-        ...scssImport,
     },
 };
 
