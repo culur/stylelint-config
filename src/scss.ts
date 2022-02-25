03856d1482bc98396f0463282f3358d9b89ecf89
phamhongphuc <phamhongphuc.info@gmail.com>
2022-02-25 17:19:26 +0700

    feat(scss): extends `stylelint-config-*-scss`

--- src/scss.ts
+++ src/scss.ts
@@ -1,9 +1,13 @@
 import type { Config } from 'stylelint';
+import configRecommendedSCSS from 'stylelint-config-recommended-scss';
+import configStandardSCSS from 'stylelint-config-standard-scss';
 import { atRuleNoUnknown } from './rules/at-rule-no-unknown';
 
 const config: Config = {
     plugins: ['stylelint-scss'],
     rules: {
+        ...configRecommendedSCSS.rules,
+        ...configStandardSCSS.rules,
         'at-rule-no-unknown': null,
         'scss/at-rule-no-unknown': atRuleNoUnknown,
     },
