9ea358240f904404a46626efc865f5237bd5cab2
phamhongphuc <phamhongphuc.info@gmail.com>
2022-02-25 17:19:09 +0700

    feat(scss): remove extends `css`

--- src/scss.ts
+++ src/scss.ts
@@ -2,7 +2,6 @@ import type { Config } from 'stylelint';
 import { atRuleNoUnknown } from './rules/at-rule-no-unknown';
 
 const config: Config = {
-    extends: ['./css'],
     plugins: ['stylelint-scss'],
     rules: {
         'at-rule-no-unknown': null,
